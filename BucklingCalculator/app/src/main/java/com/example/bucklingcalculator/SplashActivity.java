0594be1d174c1924a7358c6ecfc8370a92aa4872
lucasmari <lucasmari17@gmail.com>
2019-09-30 10:57:37 -0300

    Add Splash Activity

---
+++ BucklingCalculator/app/src/main/java/com/example/bucklingcalculator/SplashActivity.java
@@ -0,0 +1,67 @@
+package com.example.bucklingcalculator;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        setupSharedPreferences();
+
+        View progressBar = findViewById(R.id.progress_bar);
+        progressBar.setVisibility(View.VISIBLE);
+
+        Thread welcomeThread = new Thread() {
+
+            @Override
+            public void run() {
+                try {
+                    super.run();
+                    sleep(500);
+                } catch (Exception e) {
+
+                } finally {
+
+                    Intent i = new Intent(SplashActivity.this,
+                            MainActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        welcomeThread.start();
+    }
+
+    public void setupSharedPreferences() {
+        SharedPreferences sharedPreferences =
+                PreferenceManager.getDefaultSharedPreferences(this);
+        switchTheme(sharedPreferences.getBoolean(getString(R.string.switch_theme_key), false));
+    }
+
+    @Override
+    public final void setTheme(final int resid) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean theme = sharedPreferences.getBoolean(getString(R.string.switch_theme_key), false);
+
+        if (theme) {
+            super.setTheme(R.style.AppThemeDark);
+        } else {
+            super.setTheme(R.style.AppThemeLight);
+        }
+    }
+
+    public void switchTheme(boolean b) {
+        if (b) {
+            this.setTheme(R.style.AppThemeDark);
+        } else {
+            this.setTheme(R.style.AppThemeLight);
+        }
+    }
+}
